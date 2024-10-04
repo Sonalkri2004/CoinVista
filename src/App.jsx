f0f87419c7f91422f401e8a6ffdce2e488ce1069
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-10-04 22:12:02 +0530

    navbar component setup added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 
-import './App.css'
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
 
 function App() {
  
 
   return (
     <>
-      <p>hello</p>
+      
     </>
   )
 }
